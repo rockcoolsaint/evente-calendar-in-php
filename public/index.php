a947ae473d9b4bf1634c630c1e8bbfc1275cfa49
innocent onyemaenu <rockcoolsaint@gmail.com>
2015-08-10 23:11:34 +0100

    index.php updated to display calendar html

--- public/index.php
+++ public/index.php
@@ -1,14 +1,18 @@
 <?php
-/*
-* Include necessary files
-*/
-include_once '../sys/core/init.inc.php';
-/*
-* Load the calendar for January
-*/
-$cal = new Calendar($dbo, "2010-01-01 12:00:00");
+	/*
+	* Include necessary files
+	*/
+	include_once '../sys/core/init.inc.php';
+	/*
+	* Load the calendar for January
+	*/
+	$cal = new Calendar($dbo, "2010-01-01 12:00:00");
 
-if ( is_object ($cal) ) {
-echo "<pre>", var_dump($cal), "</pre>";
-}
+	//if ( is_object ($cal) ) {
+	//echo "<pre>", var_dump($cal), "</pre>";
+	//}
+	/*
+	* Display the calendar HTML
+	*/
+	echo $cal->buildCalendar();
 ?>
