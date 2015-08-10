68c310e37cb50eab5f771270084eaa4cf57429ba
innocent onyemaenu <rockcoolsaint@gmail.com>
2015-08-10 20:35:28 +0100

    file structure, database and calendar classes defined

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+* Include necessary files
+*/
+include_once '../sys/core/init.inc.php';
+/*
+* Load the calendar for January
+*/
+$cal = new Calendar($dbo, "2010-01-01 12:00:00");
+
+if ( is_object ($cal) ) {
+echo "<pre>", var_dump($cal), "</pre>";
+}
+?>
